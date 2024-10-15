abcc28599d6cc0c4a9fe04c0ed1d759109e0eeca
Fireflasher <fireflasher@proton.me>
2024-10-15 21:39:08 +0200

    minor change:  - updated readme and fixed bug
    Signed-off-by: Fireflasher <fireflasher@proton.me>

--- README.md
+++ README.md
@@ -7,30 +7,26 @@ If no list is configured for a specific Server, the Mod uses the a default List 
 ## Versioning
 
 ### Release pattern
-The mod is coded to be multi-version abroad the Minecraft version, categorised by the java version.
-The current pattern uses following scheme: <j>.<x>.<y>.<z>
-- j: java version in which this release can be used in
+The current pattern uses following scheme: <m>-<x>.<y>.<z>
+- m: minecraft version
 - x: major release version
 - y: minor release version
 - z: patch of minor release version
 
-This means, the release 17.5.0.0 can be used in Minecraft \[1.18-1.20\] as all those use the Java Version 17
-The release 21.5.0.0 can be used in \[1.21-tba\]
-
 ### Feature implementation 
-The <x>.<y>.<z> releases have the same features implemented across the <j> version.
-This means, the release 17.6.1.0 has a new feature and bug fix, which 21.5.0.0 does not have, even tho, 21 is greater than 17
+The <x>.<y>.<z> releases have the same features implemented across the <m> version.
+This means, the release 1.18.2-6.1.0 has a new feature and bug fix, which 1.20.1-5.0.0 does not have, even tho, 1.20 is newer
 
 ### Older versions
-Older versions are not multi-version thus don't have the java version tag and follow the feature pattern: <x>.<y>.<z>
-Versions before 4.2.0 can only be Configured with Modmenu from Terraformers: [github][1] or [modrinth][2]
+Versions before <m>-4.2.0 can only be Configured with Modmenu from Terraformers: [github][1] or [modrinth][2]
 
 ## Issues, Bugs or Crashes
-If you experience any Crashes or Bugs open an [Issue][3] or join the [Discord][4] . Im also always happy to improve the mod, so send me ideas of what you think would be neat to add.
+If you experience any Crashes or Bugs open an [Issue][3] or join the [Discord][4]. 
+I'm always happy to improve the mod, so send me ideas of what you think would be neat to add.
 
 ## History
-This Mod was written by me to help specifically Roleplayers, which often access the logs of their RP History if their Server is text based.
-In my Case it was Aegorin and all the people who make this Server what it is. To help them and to increase the fun and Quality of the Roleplay i started this Mod
+This Mod was written by me to help specifically Roleplayer's, which often access the logs of their interaction history if their Server is text based.
+In my Case it was Aegorin and all the people who make this Server what it is. I started this Mod to help them and to increase the fun and quality of this hobby. .
 
 If you want to check out the Server here you go: [Aegorin][5]
 But keep in mind, that the Servers only language is German
--- fabric/src/main/java/fireflasher/rplog/fabric/mixin/ChatAccessMixin.java
+++ fabric/src/main/java/fireflasher/rplog/fabric/mixin/ChatAccessMixin.java
@@ -1,11 +1,8 @@
 package fireflasher.rplog.fabric.mixin;
 
-
 import fireflasher.rplog.fabric.FabricChatLogger;
-import net.minecraft.client.multiplayer.chat.ChatListener;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.PlayerChatMessage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,41 +11,37 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 #if MC_1_18_2
 import java.util.UUID;
 import net.minecraft.client.gui.chat.StandardChatListener;
+
 @Mixin(StandardChatListener.class)
 public abstract class ChatAccessMixin {
-
     @Inject(method = "handle", at = @At("HEAD"), cancellable = true)
     public void onChatMessage(ChatType type, Component message, UUID sender, CallbackInfo ci) {
-        if(type == ChatType.CHAT) FabricChatLogger.chatFilter(message.getString());
+        if (type == ChatType.CHAT) FabricChatLogger.chatFilter(message.getString());
     }
 }
 #elif MC_1_19_2
 import net.minecraft.network.chat.PlayerChatMessage;
+import net.minecraft.client.multiplayer.chat.ChatListener;
 
 @Mixin(ChatListener.class)
 public abstract class ChatAccessMixin {
-    @Inject(method = "handleChatMessage", at = @At("HEAD"),cancellable = true)
+    @Inject(method = "handleChatMessage", at = @At("HEAD"), cancellable = true)
     public void onChatMessage(PlayerChatMessage chatMessage, ChatType.Bound bound, CallbackInfo ci) {
-        if( bound.chatType().equals(ChatType.CHAT)) FabricChatLogger.chatFilter(chatMessage.signedContent().toString());
+        if (bound.chatType().equals(ChatType.CHAT)) FabricChatLogger.chatFilter(chatMessage.signedContent().toString());
     }
 }
-
-
 #elif MC_1_20_4
 import net.minecraft.client.multiplayer.chat.ChatListener;
-
 import java.util.function.BooleanSupplier;
 import net.minecraft.client.multiplayer.chat.ChatTrustLevel;
 import com.mojang.authlib.GameProfile;
+import net.minecraft.network.chat.PlayerChatMessage;
 
 @Mixin(ChatListener.class)
 public abstract class ChatAccessMixin {
-
-    @Inject(method = "logPlayerMessage", at = @At("HEAD"),cancellable = true)
-    public void onChatMessagelogPlayerMessage(PlayerChatMessage chatMessage, ChatType.Bound bound, GameProfile gameProfile, ChatTrustLevel trustLevel, CallbackInfo ci) {
-        if( bound.chatType().equals(ChatType.CHAT)) FabricChatLogger.chatFilter(chatMessage.signedContent());
+    @Inject(method = "logPlayerMessage", at = @At("HEAD"), cancellable = true)
+    public void onChatMessage(PlayerChatMessage chatMessage, ChatType.Bound bound, GameProfile gameProfile, ChatTrustLevel trustLevel, CallbackInfo ci) {
+        if (bound.chatType().equals(ChatType.CHAT)) FabricChatLogger.chatFilter(chatMessage.signedContent());
     }
 }
-
 #endif
-
