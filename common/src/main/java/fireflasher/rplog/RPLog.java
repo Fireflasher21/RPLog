0194f52920059e86f77f6f8cc1b641eccde94f73
Fireflasher <fireflasher@proton.me>
2024-10-22 13:04:00 +0200

    code cleanup
    Signed-off-by: Fireflasher <fireflasher@proton.me>

--- common/src/main/java/fireflasher/rplog/RPLog.java
+++ common/src/main/java/fireflasher/rplog/RPLog.java
@@ -1,26 +1,12 @@
 package fireflasher.rplog;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import fireflasher.rplog.config.DefaultConfig;
-import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.Minecraft;
-import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.contents.TranslatableContents;
-import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 public class RPLog{
